119e3eb74029a51684e9b77d942c51caa27a7288
Natalka <nataliia.kolpak@gmail.com>
2024-06-03 23:28:29 +0200

    my second commit

---
+++ src/test/java/testCases/TC_008.java
@@ -0,0 +1,13 @@
+package testCases;
+
+import org.testng.annotations.Test;
+import pageObjects.HomePage;
+import testBase.BaseClass;
+
+public class TC_008 extends BaseClass {
+
+    @Test
+            public void testGit() {
+        System.out.println();
+    }
+}
